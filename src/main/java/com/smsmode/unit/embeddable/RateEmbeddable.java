5c20307cd3870b3109f2d771e0ba0a54690791ac
BENKIRANE Mokhtar <benkiranemokhtar69@gmail.com>
2025-06-26 10:04:40 +0100

    Default Rate + Standard Rates

--- src/main/java/com/smsmode/unit/embeddable/RateEmbeddable.java
+++ src/main/java/com/smsmode/unit/embeddable/RateEmbeddable.java
@@ -12,8 +12,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.math.BigDecimal;
-
 /**
  * Embeddable class representing rate information for a unit.
  *
