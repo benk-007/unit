76d154be5a22ae7c81e1856e05def94eab6164f1
Hamza HABCHI <hamza.habchi@messaging-technologies.com>
2025-07-29 21:24:13 +0100

    feat: enrich unit inventory resource with fields

--- src/main/java/com/smsmode/unit/resource/inventory/get/UnitInventoryGetResource.java
+++ src/main/java/com/smsmode/unit/resource/inventory/get/UnitInventoryGetResource.java
@@ -1,19 +1,33 @@
 package com.smsmode.unit.resource.inventory.get;
 
 import com.smsmode.unit.embeddable.BedEmbeddable;
-import com.smsmode.unit.enumeration.BedTypeEnum;
+import com.smsmode.unit.embeddable.OccupancyEmbeddable;
+import com.smsmode.unit.enumeration.AmenityEnum;
+import com.smsmode.unit.enumeration.FloorSizeUnitEnum;
 import com.smsmode.unit.enumeration.UnitNatureEnum;
 import com.smsmode.unit.resource.inventory.NightRatesResource;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Set;
 
 @Data
 public class UnitInventoryGetResource {
     private String id;
     private String name;
+    private String description;
     private UnitNatureEnum nature;
     private AvailabilityGetResource availability;
+    private Double floorSize;
+    private FloorSizeUnitEnum floorSizeUnit;
+    private OccupancyEmbeddable maxOccupancy;
+    private Set<AmenityEnum> amenities;
+    private boolean childrenAllowed = true;
+    private boolean eventsAllowed = false;
+    private boolean smokingAllowed = false;
+    private boolean petsAllowed = false;
+
+
     private List<BedEmbeddable> beds;
     private Price price;
 
@@ -24,12 +38,6 @@ public class UnitInventoryGetResource {
         private double totalAmount;
         private int minStay;
         private int maxStay;
-    }
-
-    @Data
-    public static class BedResource {
-        private BedTypeEnum type;
-        private int quantity;
     }
 
 }
